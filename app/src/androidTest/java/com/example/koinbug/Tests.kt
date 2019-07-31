5c790725a8378c9fa1544b600a248a62ca8809f9
Lorenzo Testa <kklorenzotesta@outlook.it>
2019-07-31 23:59:45 +0200

    fix test order

--- app/src/androidTest/java/com/example/koinbug/Tests.kt
+++ app/src/androidTest/java/com/example/koinbug/Tests.kt
@@ -1,18 +1,21 @@
 package com.example.koinbug
 
 import androidx.test.rule.ActivityTestRule
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runners.MethodSorters
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.koinApplication
 import org.koin.test.check.checkModules
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class Tests {
     @get:Rule
     val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun checkKoinModules() {
+    fun t1_checkKoinModules() {
         koinApplication {
             androidContext(androidx.test.platform.app.InstrumentationRegistry.getInstrumentation().context)
             modules(KoinBug.modules)
@@ -20,6 +23,6 @@ class Tests {
     }
 
     @Test
-    fun fooTest() {
+    fun t2_fooTest() {
     }
 }
